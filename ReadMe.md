c26f1def8d114a509d512939c0f3b8eb85659de5
RobertNeat <robertgoogl@gmail.com>
2025-10-05 16:48:45 +0200

    basic LD2410C handling

---
+++ ReadMe.md
@@ -0,0 +1,59 @@
+# LD2410C human presence radar
+
+This project is for setup of LD2410C radar working with ld2410 library.
+The LD2410C version is improved version from the LD2410 sensor family.
+
+Be aware that this sensor draws high amount od current, so the precautions on the power management should be considered
+
+---
+
+### Supporting library:
+- (current branch)
+https://registry.platformio.org/libraries/ncmreynolds/ld2410 
+- (more extensive library)
+https://registry.platformio.org/libraries/iavorvel/MyLD2410
+
+---
+
+### Specs:
+- manufacturer: Hi-Link
+- microwave human presence detector
+- detects moving & stationary people
+- 60° detection angle
+- FMCW radar technology (Frequency Modulated Continuous Wave)
+- bluetooth enabled
+- light level detection
+
+---
+
+### Connection diagram:
+image
+
+* out pin will have high state if it senses presence according to the sensor configuration
+---
+To configure the sensor connect:
+- throuth USB-UART and windows .exe application
+- through USB-UART and the web application
+- through android application "HLKRadarTool"
+
+---
+
+### Resource links:
+- comperhansive tutorial link:
+https://dronebotworkshop.com/ld2410c-human-sensor/
+
+- online tool for monitoring and configuration (direct USB-UART connection to sensor):
+https://ld2410.albert.nz/
+
+- link for official desktop application  (direct USB-UART connection to sensor):
+https://drive.google.com/drive/folders/1lCQv3mfHJ3XKXzweeHPFnJ_8_D_EWEKk
+
+- android bluetooth application "HLKRadarTool"
+https://play.google.com/store/apps/details?id=com.hlk.hlkradartool
+
+- official producer product website:
+https://www.hlktech.com/en/Goods-239.html
+
+
+Additional links:
+https://www.printables.com/model/767970-presence-sensor-case-esp32-c3-super-mini-ld2410c-r
---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#define MONITOR_SERIAL Serial
+#define RADAR_SERIAL Serial1
+#define RADAR_RX_PIN 20
+#define RADAR_TX_PIN 21
+
+
+#include <ld2410.h>
+
+ld2410 radar;
+
+uint32_t lastReading = 0;
+bool radarConnected = false;
+
+void setup(void)
+{
+  MONITOR_SERIAL.begin(115200); //Feedback over Serial Monitor
+  //radar.debug(MONITOR_SERIAL); //Uncomment to show debug information from the library on the Serial Monitor. By default this does not show sensor reads as they are very frequent.
+  delay(500);
+  MONITOR_SERIAL.print(F("\nConnect LD2410 radar TX to GPIO:"));
+  MONITOR_SERIAL.println(RADAR_RX_PIN);
+  MONITOR_SERIAL.print(F("Connect LD2410 radar RX to GPIO:"));
+  MONITOR_SERIAL.println(RADAR_TX_PIN);
+  MONITOR_SERIAL.print(F("LD2410 radar sensor initialising: "));
+  if(radar.begin(RADAR_SERIAL))
+  {
+    MONITOR_SERIAL.println(F("OK"));
+    MONITOR_SERIAL.print(F("LD2410 firmware version: "));
+    MONITOR_SERIAL.print(radar.firmware_major_version);
+    MONITOR_SERIAL.print('.');
+    MONITOR_SERIAL.print(radar.firmware_minor_version);
+    MONITOR_SERIAL.print('.');
+    MONITOR_SERIAL.println(radar.firmware_bugfix_version, HEX);
+  }
+  else
+  {
+    MONITOR_SERIAL.println(F("not connected"));
+  }
+}
+
+void loop()
+{
+  radar.read();
+  if(radar.isConnected() && millis() - lastReading > 1000)  //Report every 1000ms
+  {
+    lastReading = millis();
+    if(radar.presenceDetected())
+    {
+      if(radar.stationaryTargetDetected())
+      {
+        Serial.print(F("Stationary target: "));
+        Serial.print(radar.stationaryTargetDistance());
+        Serial.print(F("cm energy:"));
+        Serial.print(radar.stationaryTargetEnergy());
+        Serial.print(' ');
+      }
+      if(radar.movingTargetDetected())
+      {
+        Serial.print(F("Moving target: "));
+        Serial.print(radar.movingTargetDistance());
+        Serial.print(F("cm energy:"));
+        Serial.print(radar.movingTargetEnergy());
+      }
+      Serial.println();
+    }
+    else
+    {
+      Serial.println(F("No target"));
+    }
+  }
+}
